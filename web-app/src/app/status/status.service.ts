75aa1b04d386d0f7f1a2a03e50eabac54d5125be
Enzo Bissoli <enzogurgel15@gmail.com>
2022-05-02 11:57:56 -0300

    add(method): can now update the status list applies to status.service.ts

--- web-app/src/app/status/status.service.ts
+++ web-app/src/app/status/status.service.ts
@@ -7,4 +7,12 @@ export class StatusService {
   private headers = new Headers({ 'Content-Type': 'application/json' });
   private baseURL = 'http://localhost:3000';
   constructor(private htttp: Http) { }
+  updateStatusList(order: Status): Promise<Status> {
+    return this.htttp.put(this.baseURL + "/restaurant/status", JSON.stringify(order), {headers: this.headers})
+      .toPromise()
+      .then(res => {
+        if (res.status == 201) {return order;} else {return null;}
+      })
+      .catch(this.catch);
+  }
 }
