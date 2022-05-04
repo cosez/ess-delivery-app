453b0ebab28575f85cdd57af0a32a3452610bef3
Enzo Bissoli <enzogurgel15@gmail.com>
2022-05-04 06:58:56 -0300

    fix(newlines): .

--- server/src/status.ts
+++ server/src/status.ts
@@ -20,21 +20,26 @@ export class Status implements Pedido {
         this.id = <number> order.id;
         this.state= <number> order.state;
     }
+
     pushStatus(): void{
         Status.statusList.push(Object.assign({},this));
     }
+
     removeStatus(): void{
         Status.statusList = Status.statusList.filter(testStatus => (testStatus.id  != this.id));
     }
+
     modStatus(status_state: Status): Status{
         let index = Status.statusList.findIndex(testStatus => (testStatus.id  == this.id));
         if(Status.statusList[index].state<2)
             status_state.state = ++Status.statusList[index].state;
         return status_state;
     }
+
     returnStatus(): Status[]{
         return Status.statusList;
     }
+
     isUniqueID(): boolean {
         return !Status.statusList.some(testID => (testID.id == this.id));
     }
