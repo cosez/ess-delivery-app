4c98190f8fb2d08a37e4172a54356b9c4dbf8f2c
Leilany Ulisses <leilany.ulisses@liferay.com>
2022-05-23 04:01:05 -0300

    test-acceptance e refatoracao de features login

--- test-acceptance/stepdefinitions/login.ts
+++ test-acceptance/stepdefinitions/login.ts
@@ -22,19 +22,35 @@ defineSupportCode(function ({ Given, When, Then }) {
     })
 
 
-    Then(/^Then the login is completed and the user is redirected to the home page$/, async() => {
-        await browser.get("http://localhost:4200/login");
+    Then(/^the login is completed and the user is redirected to the home page$/, async() => {
+        await browser.get("http://localhost:4200/profile");
+
     })
 
-
-    Then(/^Then the login is not completed and an error message appears on the screen, informing us that all fields need to be filled$/, async ()=>{
-        await element(by.id("alert-fields")).isDisplayed();
+    Then(/^eu vejo uma mensagem de que o cadastro foi feito com sucesso$/, async() => {
+        await element(by.className("success_popup")).isDisplayed();
     })
 
-
-    Then(/^Then the login is not completed and an error message appears on the screen, informing us that it was not possible to find any account with the data entered$/, async () =>{
-        await element(by.id("alert-invalid")).isDisplayed();
+    Then(/^the login is not completed and an error message appears on the screen, informing us that it was not possible to find any account with the data entered$/, async () =>{
+        await element(by.className("alert-invalid")).isDisplayed();
     })
+    
+    Then(/^the login is not completed and an error message appears on the screen, informing us that all fields need to be filled$/, async ()=>{
+        await element(by.className("alert-fields")).isDisplayed();
+    })
+
+    Given(/^the password "([^\"]*)" is registered$/, async (password) =>{
+        
+    })
+
+    Given(/^the email "([^\"]*)" is registered$/, async (email) =>{
+        
+    });
+
+    Given(/^the email "([^\"]*)" is not registered$/, async (cnpj) =>{
+        
+    });
+
 
 
 })
