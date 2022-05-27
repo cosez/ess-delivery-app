552634f3c9cdbc35cf91885428489369c484a576
Enzo Bissoli <enzogurgel15@gmail.com>
2022-05-27 12:02:06 -0300

    Remoção de appControler do módulo
    App controller é uma abstração  desnecessária nesse contexto

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,14 @@ import { Module } from '@nestjs/common';
 import { StatusModule } from './status/status.module';
 import {EventEmitterModule} from '@nestjs/event-emitter';
 
-import { AppController } from './app.controller';
 
 import { AppService } from './app.service';
-import { ChangeService } from './change/change.service';
 import { StatusService } from './status/status.service';
 import { StatusGateway } from './status/status.gateway';
 
 @Module({
   imports: [StatusModule, EventEmitterModule.forRoot()  ],
   controllers: [],
-  providers: [AppService, StatusService, ChangeService, StatusGateway],
+  providers: [AppService, StatusService , StatusGateway],
 })
 export class AppModule {}
