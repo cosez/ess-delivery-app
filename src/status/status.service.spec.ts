2d1ffe827343829c378da509cd3ecd62afc21181
Enzo Bissoli <enzogurgel15@gmail.com>
2022-05-27 12:00:47 -0300

    Fix de import Status para StatusEnum
    FIX

--- src/status/status.service.spec.ts
+++ src/status/status.service.spec.ts
@@ -1,8 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StatusService } from './status.service';
-import { Order, Status } from './status.interface';
+import { Order, StatusEnum } from './status.interface';
 
-describe('This test suite describes the StatusService functionality', () => {
+describe('This test suite describes the StatusEnumService functionality', () => {
   let service: StatusService;
   let targetData: Order;
   var mockData: Order;
@@ -18,57 +18,57 @@ describe('This test suite describes the StatusService functionality', () => {
       id : 2050,
       order_name : "batata recheada",
       restaurant : "Casa da batata",
-      status : Status['Undefined']
+      status : StatusEnum['Undefined']
     }
     mockData =   {
       name : "Ana",
       id : 2050,
       order_name : "batata recheada",
       restaurant : "Casa da batata",
-      status : Status['Undefined']
+      status : StatusEnum['Undefined']
     }
   });
 
   //TODO property based testing
   describe('when a signal arrives', () => {
       it('might advance to the next state', async () => { //REFACTOR: remove async
-        targetData.status = Status['Accepted'];
-        mockData.status = Status['Undefined'];
+        targetData.status = StatusEnum['Accepted'];
+        mockData.status = StatusEnum['Undefined'];
         const out = service.promoteStatus(mockData);
         console.log(mockData,out);
         expect(out).toStrictEqual(targetData);
       })
       it('but won\'t cycle back to the first state', () => {
-        targetData.status = Status['Ready'];
-        mockData.status = Status['Ready'];
+        targetData.status = StatusEnum['Ready'];
+        mockData.status = StatusEnum['Ready'];
         const out = service.promoteStatus(mockData);
         console.log(mockData,out);
         expect(out).toStrictEqual(targetData);
       })
       it('also won\'t cycle back to the last state', () => {
-          targetData.status = Status['Rejected'];
-          mockData.status = Status['Rejected'];
+          targetData.status = StatusEnum['Rejected'];
+          mockData.status = StatusEnum['Rejected'];
           const out = service.demoteStatus(mockData);
           console.log(mockData,out);
           expect(out).toStrictEqual(targetData);
       })
       it('might return to a previous state', () => {
-          targetData.status = Status['Rejected'];
-          mockData.status = Status['Undefined'];
+          targetData.status = StatusEnum['Rejected'];
+          mockData.status = StatusEnum['Undefined'];
           const out = service.demoteStatus(mockData);
           console.log(mockData,out);
           expect(out).toStrictEqual(targetData);
       })
       it('might jump to the default state', () => {
-          targetData.status = Status['Undefined'];
-          mockData.status = Status['Preparing'];
+          targetData.status = StatusEnum['Undefined'];
+          mockData.status = StatusEnum['Preparing'];
           const out = service.resetStatus(mockData);
           console.log(mockData,out);
           expect(out).toStrictEqual(targetData);
       })
       it('might stay at the current state', () => {
-          targetData.status = Status['Undefined'];
-          mockData.status = Status['Undefined'];
+          targetData.status = StatusEnum['Undefined'];
+          mockData.status = StatusEnum['Undefined'];
           const out = service.actualStatus(mockData);
           console.log(mockData,out);
           expect(out).toStrictEqual(targetData);
