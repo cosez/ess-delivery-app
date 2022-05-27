8462c9a8be33f4055d5685f55dd937e4b2348a24
Enzo Bissoli <enzogurgel15@gmail.com>
2022-05-27 05:04:19 -0300

    Adiciona ngrx ao projeto

--- src/app/status/status.store.ts
+++ src/app/status/status.store.ts
@@ -1,19 +1,34 @@
-import { Injectable } from '@angular/core';
-import { ComponentStore } from '@ngrx/component-store';
-import { Observable } from 'rxjs';
+import {Injectable, Input} from '@angular/core';
 import { Order } from '../shared/common';
+import { ComponentStore} from '@ngrx/component-store';
+import {catchError, merge, mergeAll, Observable, pluck, } from 'rxjs';
 
-export interface OrderState {
-  order: Order[];
+export interface StatusState {
+  status: Order[];
 }
 
 @Injectable()
-export class StatusStore extends ComponentStore<OrderState>  {
+export class StatusStore extends ComponentStore<StatusState> {
 
   constructor() {
-    super({order: []});
+    super({status: []});
   }
 
-  readonly status$: Observable<Order[]> = this.select(state => state.order);
+  //Use this in the html ngFor
+  readonly $status: Observable<Order[]> = this.select(state => state.status);
+
+  readonly $giveFirst5: Observable<Order[]> = this.select(state => state.status);
+
+  readonly $giveLast10: Observable<Order[]> = this.select(state => state.status.slice(0,-10));
+
+  // update the store with new status entries
+  insertStatus = this.updater((state, status: Order) => ({
+    status: [...state.status,status],
+  }))
+
+  // remove an entry from the store
+  removeStatus = this.updater((state, status: Order) => ({
+    status: state.status.filter(x=>x!=status),
+  }))
 
 }
